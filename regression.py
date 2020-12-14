ddb06e3d6a6dc6851c8cf424135d0fe37720f5ab
Mike Jovanovich <mpjovanovich@gmail.com>
2020-12-14 00:02:56 -0600

    Added some plotting for data exploration

--- regression.py
+++ regression.py
@@ -18,24 +18,36 @@ my_date_parser = lambda x:datetime.strptime( x,'%Y-%m-%d %H:%M:%S.%f' )
 
 ## This file has the actual meter reads in KWh
 ## This is what we're trying to predict
-df = pd.read_csv( 'data/meter_reads_121997.csv', index_col=[0], parse_dates=['start_date'], date_parser=my_date_parser )
+df = pd.read_csv( 'data/meter_reads_121997.csv', parse_dates=['start_date'], date_parser=my_date_parser )
+## df = pd.read_csv( 'data/meter_reads_121997.csv', index_col=[0], parse_dates=['start_date'], date_parser=my_date_parser )
 
 ## This file has temperature in degrees Fahrenheit
-df_temp = pd.read_csv( 'data/temperature_121997.csv', index_col=[0], parse_dates=['start_date'], date_parser=my_date_parser )
+df_temp = pd.read_csv( 'data/temperature_121997.csv', parse_dates=['start_date'], date_parser=my_date_parser )
+## df_temp = pd.read_csv( 'data/temperature_121997.csv', index_col=[0], parse_dates=['start_date'], date_parser=my_date_parser )
 
 ## We will overwrite the 'df' dfframe by merging it with all of the regression variables
 df = pd.merge( df, df_temp, on='start_date' )
 
 ## A few debug statement to see how things look...
-print( df.loc[:,['temperature']].head() )
-## print( df.loc[:,['reading']].head() )
-## print( type(df.loc[:,['reading']]) )
-
-## print( df.loc[:,'temperature'].head() )
-## print( df.columns )
 ## print( df.dtypes )
 ## print( df.head() )
+## print( df.loc[:,['start_date','temperature']].head() )
+## print( df.loc[:,['start_date','reading']].head() )
 
+
+########################################################
+## Do some preliminary data exploration and visualization
+########################################################
+
+## The beginning of the set is on a holiday
+## TODO: splice dates based on querying start date column, use iloc to get readings
+plt.scatter( df.loc[1000:1500,'start_date'], df.loc[1000:1500,'reading'] )
+## plt.scatter( df.loc[1000:1500,'temperature'], df.loc[1000:1500,'reading'] )
+
+plt.xticks()
+plt.yticks()
+
+plt.show()
 
 ########################################################
 ## Create training / cross validate / test sets
@@ -49,5 +61,5 @@ print( df.loc[:,['temperature']].head() )
 ########################################################
 
 ## regr_model = linear_model.LinearRegression()
-## regr_model.fit( df.loc[:,'temperature'], df.loc[:,'reading'] )
+## regr_model.fit( df.loc[:,['temperature']], df.loc[:,['reading']] )
 
